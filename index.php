464f218496c77935348f110e20772306ec40114a
chariesa <mFSY273x4X2E>
2018-06-01 11:39:50 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My band</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
+</head>
+<body>
+<img class="logo" src="images/logo.png" alt="Logo_The_Maine">
+
+</body>
+</html>
